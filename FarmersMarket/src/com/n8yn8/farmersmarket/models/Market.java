ae8d8c49abf3899e7a7c03debd8568b09aac7bd9
Nathan <natecondell@gmail.com>
2014-05-27 19:40:55 -0700

    Working on models

--- FarmersMarket/src/com/n8yn8/farmersmarket/models/Market.java
+++ FarmersMarket/src/com/n8yn8/farmersmarket/models/Market.java
@@ -1,5 +1,86 @@
 package com.n8yn8.farmersmarket.models;
 
 public class Market {
+	
+	int _ID;
+	String name;
+	Double latitude;
+	Double longitude;
+	String days;
+	String open;
+	String close;
+	
+	public Market() {
+		super();
+	}
+
+	public Market(int id, String name, Double latitude, Double longitude,
+			String days, String open, String close) {
+		super();
+		this._ID = id;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.days = days;
+		this.open = open;
+		this.close = close;
+	}
+
+	public int getId() {
+		return _ID;
+	}
+
+	public void setId(int id) {
+		this._ID = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getDays() {
+		return days;
+	}
+
+	public void setDays(String days) {
+		this.days = days;
+	}
+
+	public String getOpen() {
+		return open;
+	}
+
+	public void setOpen(String open) {
+		this.open = open;
+	}
+
+	public String getClose() {
+		return close;
+	}
+
+	public void setClose(String close) {
+		this.close = close;
+	}
+	
 
 }
--- FarmersMarket/src/com/n8yn8/farmersmarket/models/Vendor.java
+++ FarmersMarket/src/com/n8yn8/farmersmarket/models/Vendor.java
@@ -1,5 +1,29 @@
 package com.n8yn8.farmersmarket.models;
 
 public class Vendor {
-
+	int _ID;
+	String name;
+	
+	public Vendor() {
+		
+	}
+	
+	public Vendor(int id, String name) {
+		super();
+		this._ID = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return _ID;
+	}
+	public void setId(int id) {
+		this._ID = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
